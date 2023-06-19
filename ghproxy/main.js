17dca32174ce5ee50c90f0637d61af8705b689bd
情缘 <18437931+qingyuan0o0@users.noreply.github.com>
2023-06-19 11:26:37 +0800

    Create main.js

---
+++ ghproxy/main.js
@@ -0,0 +1,31 @@
+function convert() {
+ inputStr = document.querySelector("#githubScript").value;
+ if (inputStr == "") {
+ return;
+ }
+
+ ghproxy = document.querySelector("#ghproxy").value;
+ perlcmdbegin = ' | perl -pe "$(curl -L ';
+ perlcmdend = ')"';
+ perlrule = ghproxy + 'perl-pe-para';
+
+ // 先给裸的git类链接前面加上 https://
+ inputStr = inputStr.replace(/ git/g, ' https://git');
+
+ // 再进行加github proxy的转换
+ regex = /(bash.*?)(https?:\/\/.*?)(\).*)/s;
+ replacement = '$1' + ghproxy + '$2' + perlcmdbegin + perlrule + perlcmdend + '$3';
+
+ resultStr = inputStr.replace(regex, replacement);
+
+ document.querySelector("#resultWithGhproxy").value = resultStr;
+}
+
+function copyResult() {
+ resultStr = document.querySelector("#resultWithGhproxy").value;
+ navigator.clipboard.writeText(resultStr);
+}
+
+function getLocalUrl() {
+ document.querySelector("#ghproxy").value = window.location.href;
+}
